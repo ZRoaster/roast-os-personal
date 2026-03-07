bcf115b26b645f552952d60d05ebb6009f145aad
ZRoaster <zhuz90651@gmail.com>
2026-03-08 03:10:36 +0800

    Update AppState.kt

--- app/src/main/java/com/roastos/app/AppState.kt
+++ app/src/main/java/com/roastos/app/AppState.kt
@@ -1,6 +1,13 @@
 package com.roastos.app
 
 object AppState {
+
     var lastPlannerInput: PlannerInput? = null
     var lastPlannerResult: PlannerResult? = null
+
+    var liveActualTurningSec: Int? = null
+    var liveActualYellowSec: Int? = null
+    var liveActualFcSec: Int? = null
+    var liveActualPreFcRor: Double? = null
+
 }
