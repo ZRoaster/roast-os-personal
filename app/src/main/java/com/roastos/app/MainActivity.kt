072fe4d7f71549f475b3d8c25c7f4e5dd138bf3a
ZRoaster <zhuz90651@gmail.com>
2026-03-10 14:24:28 +0800

    Update MainActivity.kt

--- app/src/main/java/com/roastos/app/MainActivity.kt
+++ app/src/main/java/com/roastos/app/MainActivity.kt
@@ -1,25 +1,11 @@
 package com.roastos.app
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.roastos.app.ui.DashboardPage
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var rootContainer: LinearLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        MachineBridge.start()
-
-        rootContainer = LinearLayout(this)
-        rootContainer.orientation = LinearLayout.VERTICAL
-
-        setContentView(rootContainer as android.view.View)
-
-        DashboardPage.show(this as Context, rootContainer)
     }
 }
