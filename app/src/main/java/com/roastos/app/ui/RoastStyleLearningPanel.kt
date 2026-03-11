6b6bbe7a33cf3254e5bf90f6a707d6ed89356f40
ZRoaster <zhuz90651@gmail.com>
2026-03-11 21:22:15 +0800

    Create RoastStyleLearningPanel.kt

---
+++ app/src/main/java/com/roastos/app/ui/RoastStyleLearningPanel.kt
@@ -0,0 +1,23 @@
+package com.roastos.app.ui
+
+import android.content.Context
+import android.widget.LinearLayout
+import com.roastos.app.RoastStyleLearningEngine
+import com.roastos.app.UiKit
+
+class RoastStyleLearningPanel(
+    context: Context
+) : LinearLayout(context) {
+
+    private val textView = UiKit.bodyText(context, "")
+
+    init {
+        orientation = VERTICAL
+        addView(textView)
+        update()
+    }
+
+    fun update() {
+        textView.text = RoastStyleLearningEngine.latestText()
+    }
+}
