0ab244141832a68ec3cdbf8f7e3109cf36203f49
ZRoaster <zhuz90651@gmail.com>
2026-03-13 23:11:59 +0800

    Update MainActivity.kt

--- app/src/main/java/com/roastos/app/MainActivity.kt
+++ app/src/main/java/com/roastos/app/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        RoastHistoryEngine.initialize(this)
+
         val restoredMachine = MachineStateStorage.load(this)
         if (restoredMachine != null) {
             RoastStateModel.machine = restoredMachine
