6554310fec98ebe6c4ba23fc6033f840db8f4c15
ZRoaster <zhuz90651@gmail.com>
2026-03-15 01:58:20 +0800

    Update MainActivity.kt

--- app/src/main/java/com/roastos/app/MainActivity.kt
+++ app/src/main/java/com/roastos/app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.roastos.app
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.roastos.app.ui.MainShellPage
@@ -22,9 +24,16 @@ class MainActivity : AppCompatActivity() {
 
         val root = LinearLayout(this).apply {
             orientation = LinearLayout.VERTICAL
+            setBackgroundColor(Color.parseColor("#F3F2EF"))
         }
 
         setContentView(root)
+
+        window.statusBarColor = Color.parseColor("#F3F2EF")
+        window.navigationBarColor = Color.parseColor("#F3F2EF")
+
+        window.decorView.systemUiVisibility =
+            window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
         MainShellPage.show(this, root)
     }
